f6192f859313cbb73247527f58bebb9de499b11a
epinopl <eryk.pasternak@gmail.com>
2025-05-25 07:54:39 +0000

    Inicjalna aplikacja z Vite + React

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Witaj w Ant Quest!</h1>
+      <p>Razem z Gruntorem i mrówkami ruszamy w podróż!</p>
+    </div>
+  );
+}
+
+export default App;
